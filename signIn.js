2d78b65d7b2d89674307e54ab91d99aacbbbbc4f
kainat555 <faryashah555@gmail.com>
2020-09-04 11:33:28 -0700

    Update changes

--- signIn.js
+++ signIn.js
@@ -1,12 +1,18 @@
 // let welcomeUser = alert("Welcome To ABC Fruit Shop!!\n Please Login To Proceed..")
 
-function signIn(){
-    window.location.href = "index.html";   
-}
-
-function pageRedirect(){
+// function signIn(){
+//     window.location.href = "index.html";
+//   }
+  
+  function pageRedirect(){
+    var name = document.getElementById("name");
+    var password = document.getElementById("password");
+    if(name.value.length === 0 || password.value.length === 0){
+      alert("Please input all fields!!");
+      return false;
+    }   
     window.location.href = "main.html";
-   }
+  }
 
 //    let name = document.getElementById("name");
 //    let user = document.getElementById("signIn").innerText;
